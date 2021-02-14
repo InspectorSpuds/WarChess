1dfb15dcd1a407731651bfcc37c0f63b86a467fc
quinngoldstein <65020016+quinngoldstein@users.noreply.github.com>
2021-02-13 16:49:14 -1000

    No update
    Worked on ChessBoard class, but deleted my little progress since was handled by Ishan and Anthony.

--- src/sample/ChessBoard.java
+++ src/sample/ChessBoard.java
@@ -1,11 +1,25 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class ChessBoard extends GridPane {
 
   public ChessBoard(int squareLengthCells) {
     super();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+      Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+
+    }
+
   }
 
 
