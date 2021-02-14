e25572df7216bdc56a82edb65bc8abdf783af300
quinngoldstein <65020016+quinngoldstein@users.noreply.github.com>
2021-02-13 17:33:19 -1000

    Piece abstract class

--- src/sample/Piece.java
+++ src/sample/Piece.java
@@ -1,1 +1,25 @@
+package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+abstract class Piece {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    }
+
+    public boolean isTaken() {
+        // if piece is taken by opponent
+    }
+
+    public boolean canMove(char next) {
+        // if piece can move to next spot
+        // check for water spots here
+        // can extend within individual piece class
+    }
+
+}
