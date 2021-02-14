5344975c8f81875a3aa00a0a71ac22c08573b5ed
quinngoldstein <65020016+quinngoldstein@users.noreply.github.com>
2021-02-13 19:14:44 -1000

    added Pawn, Knight, King classes

--- src/sample/Component.java
+++ src/sample/Component.java
@@ -12,7 +12,7 @@ public abstract class Component {
     public abstract void render(); //puts the image render of the piece on the board
 
     //moves the piece
-    public void moveTo(int col, int row){
+    public void moveTo(int col, int row) {
         this.col = col;
         this.row = row;
     }
@@ -23,4 +23,71 @@ public abstract class Component {
     }
 }
 
-public class Pawn
+public class Pawn extends Component {
+    @Override
+    public boolean canMoveTo(int col, int row) {
+
+        // checks boundaries if necessary?
+        if (this.col > 24 || this.col < 0 || this.row > 24 || this.row < 0) {
+            return false;
+        }
+
+        // covers all 8 positions
+        if ((this.col == col || this.col == col + 1 || this.col == col - 1) &&
+                (this.row = row || this.row = row + 1 || this.row = row - 1)) {
+
+            // returns false if pawn tries to stay in same position
+            if (this.col == col && this.row == row) {
+                return false;
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class Knight extends Component {
+    @Override
+    public boolean canMoveTo(int col, int row) {
+
+        // covers 2 forward, backward and 1 left, right
+        if ((this.col == col + 1 || this.col == col - 1) &&
+                (this.row == row + 2 || this.row == row - 2)) {
+            return true;
+
+        // covers 1 forward, backward and 2 left, right
+        } else if ((this.col == col + 2 || this.col == col - 2) &&
+                (this.row == row + 1 || this.row == row - 1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+public class King extends Component {
+    @Override
+    public boolean canMoveTo(int col, int row) {
+
+        // checks boundaries if necessary?
+        if (this.col > 24 || this.col < 0 || this.row > 24 || this.row < 0) {
+            return false;
+        }
+
+        // covers all 8 positions
+        if ((this.col == col || this.col == col + 1 || this.col == col - 1) &&
+                (this.row = row || this.row = row + 1 || this.row = row - 1)) {
+
+            // returns false if king tries to stay in same position
+            if (this.col == col && this.row == row) {
+                return false;
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
